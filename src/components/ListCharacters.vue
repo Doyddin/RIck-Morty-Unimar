713550374776692dbeca10421525f12348074a3d
Doyd <doydoprofissional@outlook.com>
2024-04-23 21:12:42 -0300

    Primeiro commit

---
+++ src/components/ListCharacters.vue
@@ -0,0 +1,40 @@
+<script setup>
+const character = defineProps(["name", "status", "species", "gender", "location", "image", "episode"])
+let episodios = character.episode
+</script>
+
+<template>
+    <div class="col-md-4 g-3">
+        <div class="card m-3 p-2">
+            <img :src="character.image" class="card-img-top" alt="..." height="80" />
+            <div class="card-body">
+                <div class="flex-column g-3">
+                    <p class="card-title">
+                        {{ character.name }}
+                    </p>
+                    <p>
+                        Status: {{ character.status }}
+                    </p>
+                    <p>
+                        Espécie: {{ character.species }}
+                    </p>
+                    <p>
+                        Gênero: {{ character.gender }}
+                    </p>
+                    <p>
+                        Localização Atual: {{ character.location.name }}
+                    </p>
+                    <p>
+                        Apareceu em {{ character.episode.length }} episódios
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+.card-title{
+    font-weight: bold;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { onMounted, reactive, ref } from "vue";
+import ListCharacters from "../components/ListCharacters.vue";
+
+let characters = reactive(ref());
+
+onMounted(() => {
+  fetch("https://rickandmortyapi.com/api/character?page=1")
+    .then((response) => response.json())
+    .then((response) => {
+      characters.value = response.results;
+    })
+})
+</script>
+
+<template>
+  <div class="container">
+    <div class="col-sm-12 col-md-12">
+      <div class="card">
+        <div class="card-body row">
+          <ListCharacters v-for="c in characters" :key="c.name" :name="c.name" :status="c.status" :species="c.species"
+            :gender="c.gender" :location="c.location" :image="c.image" :episode="c.episode" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
